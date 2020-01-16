12416c0182556d89ed0eba20ca4ba26610b8b675
Jordan Andersen <andersenjordan@oskyschools.us>
2020-01-16 13:00:06 -0600

    fixed tyler sorta

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
+#include <fstream>
 #include "SDL.h"
+#include "SDL_image.h"
 
 #include "Object.hpp"
 
@@ -7,30 +9,21 @@ const int SCREEN_WIDTH = 640, SCREEN_HEIGHT = 480;
 
 int init();
 
-SDL_Surface* loadImage(const char* filename) {
-    //Load tyler
-    SDL_Surface* newSurface = SDL_LoadBMP(filename);
-    if( newSurface == NULL ) {
-        printf( "Unable to load image tyler! SDL Error: %s\n", SDL_GetError() );
-        return nullptr;
-    }
-    else {
-        return newSurface;
-    }
-    
-}
-
+std::ofstream output;
 //The window we'll be rendering to
 SDL_Window* window = NULL;
-//The surface contained by the window
-SDL_Surface* gameSurface = NULL;
+//The renderer
+SDL_Renderer* renderer = NULL;
 
-int main(int argc, char* args[]) {
-    printf("Hello World!");
+SDL_Surface* imgToSurface( std::string path);
+SDL_Texture* loadTexture( std::string path );
+
+int main(int argc, char** argv) {
+    SDL_Log("Hello World!");
 
     init();
 
-    SDL_Surface* tyler = loadImage("tyler.bmp");
+    SDL_Texture* tyler = loadTexture("tyler.bmp");
 
     //Event handler
     SDL_Event e;
@@ -50,13 +43,20 @@ int main(int argc, char* args[]) {
         Object test;
         test.position.x = 5;
 
-        //Fill the surface white
-        SDL_FillRect( gameSurface, NULL, SDL_MapRGB( gameSurface->format, 0xCC, 0xCC, 0xFF ) );
+        SDL_Rect rect;
+        rect.w = 32;
+        rect.h = 32;
 
-        SDL_BlitSurface( tyler, NULL, gameSurface, NULL );
+        //Clear screen
+        SDL_RenderClear( renderer );
 
-        //Update the surface
-        SDL_UpdateWindowSurface( window );
+        //Render texture to screen
+        SDL_RenderCopy( renderer, tyler, NULL, NULL );
+
+        //Update screen
+        SDL_RenderPresent( renderer );
+        
+        //SDL_Log("hey!");
 
     }
 
@@ -66,6 +66,7 @@ int main(int argc, char* args[]) {
     //Quit SDL subsystems
     SDL_Quit();
 
+    output.close();
     return 0;
 }
 
@@ -75,23 +76,79 @@ int init() {
     int success = 0;
 
     if( SDL_Init( SDL_INIT_VIDEO ) < 0 ) {
-        printf( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );
+        SDL_Log( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );
         success = 1;
     }
     else {
     //Create window
     window = SDL_CreateWindow( "Bubbles!", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
     
-    if( window == NULL )
-    {
-        printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );
+    if( window == NULL ) {
+        SDL_Log( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );
         success = 1;
     }
+    else {
+        //Create renderer for window
+        renderer = SDL_CreateRenderer( window, -1, SDL_RENDERER_ACCELERATED );
+        if( renderer == NULL ) {
+            printf( "Renderer could not be created! SDL Error: %s\n", SDL_GetError() );
+            success = false;
+        }
+        else {
+            //Initialize renderer color
+            SDL_SetRenderDrawColor( renderer, 0xFF, 0xFF, 0xFF, 0xFF );
 
-    //Get window surface
-    gameSurface = SDL_GetWindowSurface( window );
+            //Initialize PNG loading
+            int imgFlags = IMG_INIT_PNG;
+            if( !( IMG_Init( imgFlags ) & imgFlags ) )
+            {
+                printf( "SDL_image could not initialize! SDL_image Error: %s\n", IMG_GetError() );
+                success = false;
+            }
+        }
+    }
     
     }
 
     return success;
+}
+
+
+SDL_Texture* loadTexture( std::string path )
+{
+    //The final texture
+    SDL_Texture* newTexture = NULL;
+
+    //Load image at specified path
+    SDL_Surface* loadedSurface = imgToSurface( path );
+
+    //Create texture from surface pixels
+    newTexture = SDL_CreateTextureFromSurface( renderer, loadedSurface );
+    if( newTexture == NULL )
+    {
+        SDL_Log( "Unable to create texture from %s! SDL Error: %s\n", path.c_str(), SDL_GetError() );
+    }
+
+    //Get rid of old loaded surface
+    SDL_FreeSurface( loadedSurface );
+
+    return newTexture;
+}
+
+SDL_Surface* imgToSurface( std::string path) {
+    //Load surface
+    SDL_Surface* newSurface;
+    size_t dotPos = path.find_last_of(".");
+    std::string extension = path.substr(dotPos);
+
+    printf(extension.c_str());
+
+    newSurface = SDL_LoadBMP(path.c_str());
+    if( newSurface == NULL ) {
+        SDL_Log( "Unable to load image %s! SDL Error: %s\n", path.c_str(), SDL_GetError() );
+        return nullptr;
+    }
+    else {
+        return newSurface;
+    }
 }
